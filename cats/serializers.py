a210fa3ecb63efe9de1e61b92e613aa98db6bdc3
boreesych <bartenev@outlook.com>
2021-06-13 01:49:41 +0100

    added ref_name for UserSerializer

--- cats/serializers.py
+++ cats/serializers.py
@@ -12,6 +12,7 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('id', 'username', 'first_name', 'last_name', 'cats')
+        ref_name = 'ReadOnlyUsers'
 
 
 class AchievementSerializer(serializers.ModelSerializer):
